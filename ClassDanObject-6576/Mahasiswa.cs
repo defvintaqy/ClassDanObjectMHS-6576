a67fa5f41de62a04d76b6d1be98c2aac678e589d
LENOVO <LENOVO@DEFVINTAQY>
2026-04-15 23:02:03 +0700

    Add project files.

---
+++ ClassDanObject-6576/Mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDanObject_6576
+{
+    public class Mahasiswa
+    {
+        public string Nim {  get; set; }
+        public  string Nama { get; set; }
+        public float Ipk { get; set; }
+
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("NIM: {0}", Nim);
+            Console.WriteLine("IPK:{0}", Ipk);
+            Console.WriteLine("\nTelah melakukakn regestrasi");
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs", Nama);
+        }
+
+    }
+}
---
+++ ClassDanObject-6576/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDanObject_6576
+{
+     class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.Nim = "25.11.6576";
+            saya.Nama = "Defvin";
+            saya.Ipk = 4.0f;
+
+
+            kamu.Nim = "6576";
+            kamu.Nama = "Taqiy";
+            kamu.Ipk = 3.5f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            Console.WriteLine();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+
+        }
+    }
+}
